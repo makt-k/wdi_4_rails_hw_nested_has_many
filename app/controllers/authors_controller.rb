ef1125f52ea043f4d6875526550ff5f0197a243f
K Mak <karen.mak@stx.oxon.org>
2014-01-29 18:11:07 -0500

    author index controller and view completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,26 @@
 class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(params[author_params])
+    @author.save
+  end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :email)
+  end
+
 end
