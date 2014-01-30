8bceaefefa75d1ac40d3aaee06906c0aa570f4fd
K Mak <karen.mak@stx.oxon.org>
2014-01-29 19:26:36 -0500

    new article controller, view, and button added with specs passed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+  before_action :get_author
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @author.articles << Article.create!(article_params)
+    redirect_to author_path(@author.id)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author_id)
+  end
+
+  def get_author
+    @author = Author.find(params[:author_id]) if params.key?(:author_id)
+  end
+
 end
