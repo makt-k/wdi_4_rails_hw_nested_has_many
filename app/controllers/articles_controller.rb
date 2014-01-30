d94a6b00a870eaa4dc5ed04075be4c75c28cc771
K Mak <karen.mak@stx.oxon.org>
2014-01-29 19:36:56 -0500

    root with listing of all articles (with the most recent at the top), with their title,  author's name, and a short snippet of the body

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :get_author
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order(updated_at: :desc)
   end
 
   def new
