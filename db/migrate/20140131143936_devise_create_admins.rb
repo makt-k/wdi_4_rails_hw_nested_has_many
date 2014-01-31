03fa19e829031260fb92acb1afd9ac248be545ee
K Mak <karen.mak@stx.oxon.org>
2014-01-31 10:03:19 -0500

    added user login with Devise

--- db/migrate/20140131143936_devise_create_admins.rb
+++ db/migrate/20140131143936_devise_create_admins.rb
@@ -1,13 +1,13 @@
 class DeviseCreateAdmins < ActiveRecord::Migration
   def change
     create_table(:admins) do |t|
-      ## Database authenticatable
+      ## Database authenticatable (required)
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # ## Recoverable
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -35,7 +35,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration
     end
 
     add_index :admins, :email,                :unique => true
-    add_index :admins, :reset_password_token, :unique => true
+    # add_index :admins, :reset_password_token, :unique => true
     # add_index :admins, :confirmation_token,   :unique => true
     # add_index :admins, :unlock_token,         :unique => true
   end
